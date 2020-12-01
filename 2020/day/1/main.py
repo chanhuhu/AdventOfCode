7ed850dd6c7eec4d0368e741512bb5f49da94eae
Aphimuk Yimsukpaitoon <luroman6@gmail.com>
2020-12-01 17:03:18 +0700

    day1 solution

---
+++ 2020/day/1/main.py
@@ -0,0 +1,51 @@
+import itertools
+import unittest
+
+def openfile(filename: str):
+
+    with open(filename, 'r') as f:
+        entries = []
+        for i in f.read().split("\n"):
+            if i:
+                entries.append(int(i))
+
+    return entries
+
+def day1a(filename: str):
+    file = openfile(filename=filename)
+
+    for a,b in itertools.permutations(file, 2):
+        target: int = 2020 - a
+        if target == b:
+            return a * b
+
+    return -1
+
+def day1b(filename: str):
+    file = openfile(filename=filename)
+
+    for a,b,c in itertools.permutations(file, 3):
+
+        if a + b + c == 2020:
+            return a * b * c
+
+class Test(unittest.TestCase):
+
+    def test_day1a_example(self):
+        file = 'a.input.example.txt'
+        result = 514579
+        self.assertEqual(result, day1a(file))
+
+    def test_day1a(self):
+        file = 'a.input.txt'
+        result = 1020099
+        self.assertEqual(result, day1a(file))
+
+    def test_day1b(self):
+        file = 'b.input.txt'
+        result = 1020099
+        print(day1b(file))
+        self.assertEqual(result, day1a(file))
+
+if __name__ == '__main__':
+    unittest.main()
