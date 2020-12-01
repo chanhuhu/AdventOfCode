50a6eb4c7e9b519ca373ec9f3b2950e05ef40adc
Aphimuk Yimsukpaitoon <luroman6@gmail.com>
2020-12-01 23:33:15 +0700

    fix: changed forloop to list comprehensions.

--- 2020/day/1/main.py
+++ 2020/day/1/main.py
@@ -1,53 +1,55 @@
 import itertools
 import unittest
 
+
 def openfile(filename: str):
 
-    with open(filename, 'r') as f:
-        entries = []
-        for i in f.read().split("\n"):
-            if i:
-                entries.append(int(i))
+    with open(filename, "r") as f:
+        entries = [int(i) for i in f.read().split("\n") if i]
 
     return entries
+
 
 def day1a(filename: str):
     file = openfile(filename=filename)
 
-    for a,b in itertools.permutations(file, 2):
+    for a, b in itertools.permutations(file, 2):
         target: int = 2020 - a
         if target == b:
+            print(a * b)
             return a * b
 
     return -1
 
+
 def day1b(filename: str) -> int:
     file = openfile(filename=filename)
 
-    for a,b,c in itertools.permutations(file, 3):
+    for a, b, c in itertools.permutations(file, 3):
 
         if a + b + c == 2020:
             return a * b * c
 
     return -1
 
-class Test(unittest.TestCase):
 
+class Test(unittest.TestCase):
     def test_day1a_example(self):
-        file = 'a.input.example.txt'
+        file = "a.input.example.txt"
         result = 514579
         self.assertEqual(result, day1a(file))
 
     def test_day1a(self):
-        file = 'a.input.txt'
+        file = "a.input.txt"
         result = 1020099
         self.assertEqual(result, day1a(file))
 
     def test_day1b(self):
-        file = 'b.input.txt'
+        file = "b.input.txt"
         result = 1020099
         print(day1b(file))
         self.assertEqual(result, day1a(file))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
